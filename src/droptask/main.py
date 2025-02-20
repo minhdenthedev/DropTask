aa6c73033ae2d75048e66399956be8ce645d3360
minhdenthedev <minhdenthedev@github.com>
2025-02-20 15:41:25 +0700

    Update

--- src/droptask/main.py
+++ src/droptask/main.py
@@ -1,5 +1,22 @@
-from src.droptask import services
-from src.droptask.cli import cli
+import sys
+from PySide6.QtWidgets import QMainWindow, QApplication, QPushButton
+from src.droptask.ui.mainwindow import Ui_MainWindow
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+        self.ui.iconOnlySideBarWidget.hide()
+        self.ui.stackedWidget.setCurrentIndex(0)
+        self.ui.menuButton.setChecked(False)
+
 
 if __name__ == '__main__':
-    cli()
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+
+    sys.exit(app.exec())
